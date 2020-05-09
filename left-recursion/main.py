cd6a14e6711a489db6f27deaf867b63cac22eb90
Farid <difcortesgu@unal.edu.co>
2020-05-09 17:54:23 -0500

    :construction: Algoritmo para eliminar factores comunes por izquierda

--- left-recursion/main.py
+++ left-recursion/main.py
@@ -1,4 +1,5 @@
 from grammar import grammar
+# from test_grammar import grammar
 import copy
 
 def isNonTerminal(simbol):
@@ -49,15 +50,64 @@ def removeLeftRecursion(grammar):
         grammar = removeDirectLeftRecursion(grammar, Ai)
     return grammar
 
-# def removeLeftCommonFactors(grammar):
-#     parseF = {k:i for i,(k,v) in enumerate(grammar.items())}
-#     parseB = {i:k for i,(k,v) in enumerate(grammar.items())}
-#     for i in range(len(grammar)):
-#         Ai = parseB[i]
-#         rules = grammar[Ai]
-#         for index, ai in enumerate(rules[1]):
-#             pass
-#     return grammar
+def get_n_common_factors(value):
+    hasMoreFactors = True
+    n_common_factors = 0
+    while hasMoreFactors:
+        hasMoreFactors = False
+        if n_common_factors < len(value[0][1]):
+            factor = value[0][1][n_common_factors]
+            n_common_factors += 1
+            hasMoreFactors = True
+            for rule in value:
+                if n_common_factors > len(rule[1]) or rule[1][n_common_factors-1] != factor:
+                    n_common_factors -= 1
+                    hasMoreFactors = False
+                    break
+    return n_common_factors
+
+def getNextFactors(rules):
+    factors = {}
+    for i, rule in enumerate(rules):
+        if rule[0] not in factors:
+            factors[rule[0]] = []
+        factors[rule[0]].append((i, rule[1:]))
+    factor = max(factors, key = lambda k: len(factors[k]))
+    if len(factors[factor]) > 1:
+        return factors[factor]
+    return None
+
+def removeNCommonFactors(grammar, A, factors, n, A_):
+    if not factors: return grammar
+    factor = grammar[A][1][factors[0][0]][:n]
+    aes = []
+    j = 0
+    for i,value in factors:
+        aes.append(grammar[A][1][i-j][n:])
+        del grammar[A][1][i - j]
+        j+=1
+    factor.append(A_)
+    grammar[A][1].append(factor)
+    grammar[A_] = [[],[],[],[]]
+    for a in aes:
+        grammar[A_][1].append(a)
+    return grammar
+
+def removeLeftCommonFactors(grammar):
+    parseB = {i:k for i,(k,v) in enumerate(grammar.items())}
+    for i in range(len(grammar)):
+        Ai = parseB[i]
+        rules = grammar[Ai][1]
+        factor = getNextFactors(rules)
+        fi = 1
+        while factor != None:
+            n_common_factors = get_n_common_factors(factor) + 1
+            grammar = removeNCommonFactors(grammar, Ai, factor,  n_common_factors, Ai+"-"*(fi))
+            factor = getNextFactors(rules)
+            fi+=1
+    return grammar
 
 grammar = removeLeftRecursion(grammar)
-print("hola")
+# grammar = removeLeftCommonFactors(grammar)
+print(grammar)
+
